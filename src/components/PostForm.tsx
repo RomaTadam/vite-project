fa17e75f542b22e17e6a63df581f3a1fc68192a1
Roman Levakhin <r.levakhin@itfb.com>
2024-12-06 17:10:37 +0400

    ffff

---
+++ src/components/PostForm.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import MyInput from './input/MyInput'
+import MyButton from './button/MyButton'
+
+export default function PostForm({create}) {
+  // console.log('PostForm');
+  // console.log({create});
+
+  const [toBuy, setToBuy] = useState({product: ""});
+
+  const addNew = (e) => {
+    console.log('addNew');
+    console.log(e);
+
+    e.preventDefault();
+    const newNew = {
+      ...toBuy, id: Date.now()
+    }
+    create(newNew)
+    setToBuy({product: ""})
+  }
+    
+  return (
+    <form>
+      <MyInput
+        value={toBuy.product}
+        onChange={e => setToBuy({...toBuy, product: e.target.value})}
+        type="text" 
+        placeholder='Введите покупку' 
+      />
+      <MyButton onClick={addNew}>Добавить покупку</MyButton>
+    </form>
+  )
+}
